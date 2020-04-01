803117be4572fd0b38053ffa724f9e6d6386e485
Joshua Ellinger <josh@exemplartech.com>
2020-03-31 19:04:39 -0500

    rewrote Result log to use enums and a single list

--- result_log.py
+++ result_log.py
@@ -1,11 +1,39 @@
 # ResultLog -- collects results for a run and handle printing/posting them
+#
+# Each message is assigned a category
+#
+from enum import Enum
 import json
 import io
 import pandas as pd
 import numpy as np
 import udatetime
-from typing import Tuple
+from typing import Tuple, Dict, List
 import time
+import html
+
+class ResultCategory(Enum):
+    ERROR = "error"
+    WARNING = "warning"
+    INFO = "info"
+
+class ResultMessage:
+
+    __slots__ = (
+        'category',
+        'location',
+        'message',
+        'ms'
+    )
+
+    def __init__(self, category: ResultCategory, location: str, message: str, ms: int):
+        self.category = category
+        self.location = location
+        self.message = message
+        self.ms = ms
+
+    def to_dict(self) -> Dict:
+        return { "category": self.category.name, "location": self.location, "message": self.message, "ms": self.ms }
 
 class ResultLog():
 
@@ -13,27 +41,31 @@ class ResultLog():
         self.loaded_at = udatetime.now_as_eastern()
         self.start = time.process_time_ns()
 
-        self._infos = []
-        self._warnings = []
-        self._errors = []
+        self._messages: List[ResultLog] = []
 
-    def _make(self, location: str, message: str) -> Tuple[str, str, int]:
+    @property
+    def messages(self) -> List[ResultMessage]:
+        return self._messages
+    
+    def by_category(self, category: ResultCategory) -> List[ResultMessage]:
+        return [x for x in self._messages if x.category == category]
+
+    def add(self, category: ResultCategory, location: str, message: str) -> None:
+        if message is None: raise Exception("Missing message")
+
         end = time.process_time_ns()
         delta_ms = int((end - self.start) * 1e-6)
         self.start = end
-        return (location, message, delta_ms)
 
-    def info(self, location: str, message: str) -> None:
-        if message is None: raise Exception("Missing message")
-        self._infos.append(self._make(location, message))
-
-    def warning(self, location: str, message: str) -> None:
-        if message is None: raise Exception("Missing message")
-        self._warnings.append(self._make(location, message))
+        msg = ResultMessage(category, location, message, delta_ms)
+        self._messages.append(msg)
 
     def error(self, location: str, message: str) -> None:
-        if message is None: raise Exception("Missing message")
-        self._errors.append(self._make(location, message))
+        self.add(ResultCategory.ERROR, location, message)
+    def warning(self, location: str, message: str) -> None:
+        self.add(ResultCategory.WARNING, location, message)
+    def info(self, location: str, message: str) -> None:
+        self.add(ResultCategory.INFO, location, message)
 
     # -----
 
@@ -41,58 +73,43 @@ class ResultLog():
 
         print("")
 
-        if len(self._errors) == 0 and len(self._warnings) == 0 and len(self._infos) == 0:
+        if len(self._messages) == 0:
             print("[No Messages]")
 
-        def format_ms(ms: int):
-            return f" ({ms}ms)" if ms > 100 else ""
-
-        if len(self._errors) > 0:
-            print("=====| ERROR |===========")
-            for location, message, ms in self._errors:
-                print(f"{location}: {message}{format_ms(ms)}")
-
-        if len(self._warnings) > 0:
-            print("\n=====| WARNING |===========")
-            for location, message, ms in self._warnings:
-                print(f"{location}: {message}{format_ms(ms)}")
-
-        if len(self._infos) > 0:
-            print("\n=====| INFO |===========")
-            for location, message, ms in self._infos:
-                print(f"{location}: {message}{format_ms(ms)}")
+        for cat in ResultCategory:
+            messages = self.by_category(cat)
+            if len(messages) == 0: continue
+        
+            print(f"=====| {cat.name} |===========")
+            for x in messages:
+                print(f"{x.location}: {x.message}")
 
         print("")
 
 
     def to_json(self) -> str:
-
         result = {
-            "infomation": [ { "location": x[0], "message": x[1], "ms": x[2] } for x in self._infos ],
-            "warning": [ { "location": x[0], "message": x[1], "ms": x[2] } for x in self._warnings ],
-            "error": [ { "location": x[0], "message": x[1], "ms": x[2] } for x in self._errors ],
+            "error": [ x.to_dict() for x in self._messages if x.category == ResultCategory.ERROR ],
+            "warning": [ x.to_dict() for x in self._messages if x.category == ResultCategory.WARNING ],
+            "infomation": [ x.to_dict() for x in self._messages if x.category == ResultCategory.INFO ],
         }
         return json.dumps(result, indent=2)
 
 
     def to_frame(self) -> pd.DataFrame:
  
-        n_total = len(self._infos) + len(self._warnings) + len(self._errors)
+        n_total = len(self._messages)
         level = np.zeros(n_total, dtype=object)
         location = np.zeros(n_total, dtype=object)
         message = np.zeros(n_total, dtype=object)
         time_ms = np.zeros(n_total, dtype=np.int)
 
         idx = 0
-        for loc, msg, ms in self._errors:
-            level[idx], location[idx], message[idx], time_ms[idx] = "ERROR", loc, msg, ms
-            idx += 1  
-        for loc, msg, ms in self._warnings:
-            level[idx], location[idx], message[idx], time_ms[idx] = "WARNING", loc, msg, ms
-            idx += 1  
-        for loc, msg, ms in self._infos:
-            level[idx], location[idx], message[idx], time_ms[idx] = "INFO", loc, msg, ms
-            idx += 1  
+        for cat in ResultCategory:
+            messages = self.by_category(cat)
+            for x in messages:
+                level[idx], location[idx], message[idx], time_ms[idx] = cat.name, x.location, x.message, x.ms
+                idx += 1
 
         df = pd.DataFrame({
             "level": level, "location": location, "message": message, "ms": time_ms
@@ -105,20 +122,43 @@ class ResultLog():
         df.to_csv(dest, index=False)
         return dest.getvalue()
     
-    def to_html(self) -> str:
-        df = self.to_frame()
+    def to_html(self, as_fragment=False) -> str:
+        lines = []
 
-        # didn't have time to finish - Josh
-        # want a css class per row
-        #
-        # # https://towardsdatascience.com/style-pandas-dataframe-like-a-master-6b02bf6468b0
-        #def row_class(r):
-        #     c = f"class: " + r.level.lower()
-        #     return [c,c,c]
-        #result = df.style.apply(row_class, axis=1).render() 
+        if not as_fragment:
+            lines.append("<html>")
+            lines.append("  <head>")
+            lines.append("    <link rel='stylesheet' href='result_log.css' type='text/css' />")
+            lines.append("  </head>")
+            lines.append("  <body>")
 
-        result = df.to_html()
-        return result
+        lines.append("    <table class='result-table'>")
+
+        lines.append(f"      <tr>")
+        lines.append(f"        <th class='category'>Category</td>")
+        lines.append(f"        <th class='location'>Location</td>")
+        lines.append(f"        <th class='message'>Message</td>")
+        #lines.append(f"        <th class='ms'>time (ms)</td>")
+        lines.append(f"      </tr>")
+
+        for cat in ResultCategory:
+            messages = self.by_category(cat)
+            for x in messages:
+                lines.append(f"      <tr class='{cat.name}'>")
+                lines.append(f"        <td class='category'>{x.category}</td>")
+                lines.append(f"        <td class='location'>{x.location}</td>")
+                msg = html.escape(x.message).replace('\n', '<br>\n')
+                lines.append(f"        <td class='message'>{msg}</td>")
+                #lines.append(f"        <td class='ms'>{x.ms}</td>")
+                lines.append(f"      </tr>")
+
+        lines.append("    </table>")
+
+        if not as_fragment:
+            lines.append("  </body>")
+            lines.append("</html>")
+
+        return "\n".join(lines)
 
 
 # -----------------------------
