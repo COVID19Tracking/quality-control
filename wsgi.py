ea82d280cd8ab592e52bee6f2f6632c5b791597d
Joshua Ellinger <josh@exemplartech.com>
2020-04-04 08:55:11 -0500

    gunicorn needs app as global

--- wsgi.py
+++ wsgi.py
@@ -6,20 +6,20 @@ from flask import Flask
 from flaskapp import create_app
 from app.webhooks import webhook
 
+app = create_app()
 
 if __name__ == "__main__":
 
-    app = create_app()
-
     # propogate logging
-    #gunicorn_logger = logging.getLogger("gunicorn.error")
-    #if gunicorn_logger != None:
-    #    app.logger.handlers = gunicorn_logger.handlers
-    #    app.logger.setLevel(gunicorn_logger.level)
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    if gunicorn_logger != None:
+        app.logger.handlers = gunicorn_logger.handlers
+        app.logger.setLevel(gunicorn_logger.level)
     
     # for auto-update
     app.config['GITHUB_SECRET'] = os.environ.get('GITHUB_SECRET')
     app.config['REPO_PATH'] = os.environ.get('REPO_PATH')
     app.register_blueprint(webhook)
 
+    logger.info("running from wsgi")
     app.run()
